4e06f61f39986031650639c07e6b270c15acb20d
Jared D. Baker <jbaksta@gmail.com>
2016-10-15 22:54:33 -0600

    added functions.py

---
+++ intro/functions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+Functions
+"""
+
+import sys
+
+def add2(a):
+    return a+2
+    
+def multi2(a):
+    return a*2
+    
+def main():
+    a = 1
+    
+    print(add2(a))
+    print(multi2(a))
+    
+    return 0
+    
+if __name__ == "__main__":
+    main()
